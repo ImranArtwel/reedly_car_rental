1af819b6e04baca92edc1e4a01ce9b4e5b236fb5
artwel <imranartweljunior@gmail.com>
2020-05-16 12:41:56 -0500

    Fixed logo

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.styles.scss";
+
+const Footer = () => (
+  <div className="footer">
+    <p>&copy; 2020. Reedly Cars. All Rights Reserved</p>
+  </div>
+);
+
+export default Footer;
